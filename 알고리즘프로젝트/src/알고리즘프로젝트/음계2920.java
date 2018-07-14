2552fd4ac6e6921acc6e00ecb6d0c94aa53454c8
dreammarker <35982719+dreammarker@users.noreply.github.com>
2018-07-14 19:47:57 +0900

    알고리즘프로젝트

--- 알고리즘프로젝트/src/알고리즘프로젝트/음계2920.java
+++ 알고리즘프로젝트/src/알고리즘프로젝트/음계2920.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class 음계2920 {
 public static void main(String[] args) throws IOException {
 	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 	StringTokenizer st = new StringTokenizer(br.readLine());
